a77a8db91340192657ad5e64042340b26bbbdbd8
bhagatyogita <bhagatyogita1996@gmail.com>
2025-12-27 22:57:38 +0530

    Automated user story name

---
+++ src/com/example/C.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Automate testcase no. tc006");
+	}
+
+}
